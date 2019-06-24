f0359a6d3b9eb4fa622f9e8d4a1aba419c962869
kupriknikita <kupriknikita@gmail.com>
2019-06-24 11:08:46 +0300

    Create project structure

---
+++ RxSwiftSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftSample
+//
+//  Created by Nikita Kuprik on 6/24/19.
+//  Copyright © 2019 Nikita Kuprik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
